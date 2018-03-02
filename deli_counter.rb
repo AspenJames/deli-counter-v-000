66ff5fd90be7b9f339b07b15328ff68698a35cfb
Aspen James <aspenjames@tqca.org>
2018-03-02 21:13:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 def line(deli_line)
-  if deli_line.nil?
+  if deli_line == []
     puts "The line is currently empty"
   else
     spot = nil
@@ -9,5 +9,5 @@ def line(deli_line)
       line_with_spot << "#{spot}. #{name}"
     }
     puts "The line is currently: #{line_with_spot.join(" ")}"
-  end 
+  end
 end
