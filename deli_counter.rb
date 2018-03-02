7dc1d994bdb59354879db8d6a0c40725cb1be526
Aspen James <aspenjames@tqca.org>
2018-03-02 21:29:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,6 @@ def line(deli_line)
   if deli_line == []
     puts "The line is currently empty."
   else
-    # spot = nil
     line_with_spot = Array.new
     deli_line.each_with_index { |name, index|
       spot = index + 1
