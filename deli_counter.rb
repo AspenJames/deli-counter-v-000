65814c5855835af2e00326798cc90b19752ab2fd
Aspen James <aspenjames@tqca.org>
2018-03-02 21:13:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(deli_line)
+  if deli_line.nil?
+    puts "The line is currently empty"
+  else
+    spot = nil
+    line_with_spot = Array.new
+    deli_line.each_with_index { |name, index|
+      spot = index + 1
+      line_with_spot << "#{spot}. #{name}"
+    }
+    puts "The line is currently: #{line_with_spot.join(" ")}"
+  end 
+end
